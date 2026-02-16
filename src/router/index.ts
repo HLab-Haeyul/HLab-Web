09b2fbd27289eb630a16bff5bc4a58b84056ec14
Haeyul <kimminje661@outlook.kr>
2026-02-16 14:37:06 +0900

    feat(#3) : 한국어판 영어판 포트폴리오 라우팅 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PortfolioView from '../views/PortfolioView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      redirect: '/ko',
+    },
+    {
+      path: '/ko',
+      name: 'portfolio-ko',
+      component: PortfolioView,
+      meta: {
+        locale: 'ko',
+      },
+    },
+    {
+      path: '/en',
+      name: 'portfolio-en',
+      component: PortfolioView,
+      meta: {
+        locale: 'en',
+      },
+    },
+  ],
 })
 
 export default router
